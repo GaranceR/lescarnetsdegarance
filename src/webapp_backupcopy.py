0d3c11d15627c16533eb342655e0326fdad63ef9
GaranceR <garance.recalde@gmail.com>
2015-11-23 16:13:34 +0000

    Trying to fix Create an account redirection problem / weird 500 error

--- src/webapp_backupcopy.py
+++ src/webapp_backupcopy.py
@@ -11,6 +11,7 @@ from contextlib import closing
 
 #Create the app
 app = Flask(__name__)
+#db_location = 'var/database.db'
 
 #Functions
 
@@ -54,9 +55,20 @@ def connect_db():
   return conn
   #return sqlite3.connect(app.config['database'])
 
+#def get_db():
+  #db = getattr(g, 'db', None)
+  #if db is None:
+   # db = sqlite3.connect(db.location)
+    #g.db = db
+  #return db
+  #if not hasattr(g, 'sqlite_db'):
+   #   g.sqlite_db = connect_db()
+   # return g.sqlite_db
 
 def init_db():
   with closing(connect_db()) as db:
+  #with app.app_context():
+  #db = get_db()
     with app.open_resource('schema.sql', mode='r') as f:
       db.cursor().executescript(f.read())
     db.commit()
@@ -76,29 +88,15 @@ def teardown_request(exception):
 @app.route('/display_users')
 def display_users():
   cur = g.db.cursor()
-  #INSERT in DB
   cur.execute('INSERT INTO user (name_user, password_user, email_user)\
-  VALUES ("test","pswd","email_test")')
+  VALUES ("test","pswd","email_test"))
   g.db.commit()
   cur = g.db.execute('SELECT name_user, email_user FROM user ORDER BY id_user ASC')
+  #cur.execute('INSERT INTO user (name_user, password_user, email_user) VALUES \
+  #(?,?,?)',
+  #[request.form['username'],request.form['password'],request.form['email']])
   entries = [dict(name_user=row[0], email_user=row[1]) for row in cur.fetchall()]
   return render_template('index.html',entries=entries)
-
-@app.route('/createaccount/', methods=['GET','POST'])
-def createaccount():
-  #error = None
-  if request.method == 'POST':
-    return "Hello"
-    cur = g.db.cursor()
-    #INSERT in DB
-    cur.execute('INSERT INTO user (name_user, password_user, email_user) VALUES \
-    (?,?,?)',[request.form['username'],request.form['password'],request.form['email']])
-    g.db.commit()
-    #not working
-    #flash('Your account was successfully created!')
-    #return redirect(url_for('display_users'))
-  else:
-    return render_template('createaccount.html')
 
 @app.route('/')
 @app.route('/home/')
@@ -252,6 +250,44 @@ def private():
   so redirect to login URL"""
   return redirect(url_for('login'))
 
+@app.route('/createaccount/', methods=['GET','POST'])
+def createaccount():
+  #error = None
+  if request.method == 'POST':
+    cur = g.db.cursor()
+    #INSERT in DB
+    cur.execute('INSERT INTO user (name_user, password_user, email_user) VALUES \
+    (?,?,?)',[request.form['username'],request.form['password'],request.form['email']])
+    g.db.commit()
+    #flash('Your account was successfully created!')
+    #return redirect(url_for('display_users'))
+    #return render_template()
+  return render_template('createaccount.html')
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+  error = None
+  if request.method == 'POST':
+    print request.form #for me to see it in the console
+    username = request.form['username']
+    password = request.form['password']
+
+  query = 'SELECT * FROM user WHERE name_user = ?'
+  testname = query_db(query, [request.form['username']])
+  #if testname:
+
+ # else:
+    #error = "Sorry there is no account with this username. Make sure you spelt it right"
+    #elif password doesnt match
+    #error = "Sorry, your password isn't the one you provided. Try again."
+  msg = "Hi %s" % username + "<br/>It's nice to see you!"
+  return msg #return a template instead
+    #else:
+    #session['logged in'] = True
+    #flash('You just logged in')
+    #return redirect(url_for('home'))
+
+  return render_template('login.html',error=error)
 
 #Error handling
 
